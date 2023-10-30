a3afb27a191e34a8a8d3411bd84d5425b1e0f2a1
Fidjanis <144933049+Fidjanis@users.noreply.github.com>
2023-10-30 13:31:09 +0300

    Add files via upload

---
+++ бинарка/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,101 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+#include <stdexcept>
+
+using namespace std;
+
+const int NAME_SIZE = 25;
+
+struct Request {
+    int client_id;
+    char name[NAME_SIZE];
+    double height;
+    double weight;
+};
+
+struct Answer {
+    int client_id;
+    double IMT;
+};
+
+void writeRequest(const string& fileName, Request& request) {
+    fstream f(fileName, ios::binary | ios::app);
+    if (!f) {
+        throw runtime_error("Файл не открылся");
+    }
+    f.write(reinterpret_cast<const char*>(&request), sizeof(request));
+    f.close();
+}
+
+Answer getLastAnswer(const string& fileName, long currentSize) {
+    size_t size_of_answer = sizeof(Answer);
+    long last_answer_position = currentSize - size_of_answer;
+
+    fstream f(fileName, ios::in | ios::binary);
+    if (!f) {
+        throw runtime_error("Файл не открылся");
+    }
+
+    Answer lastAnswer;
+    f.seekg(last_answer_position);
+    f.read((char*)&lastAnswer, sizeof(Answer));
+    f.close();
+    return lastAnswer;
+}
+
+long getSizeOfFile(const string& fileName) {
+    fstream f(fileName, ios::binary | ios::app);
+    if (!f) {
+        throw runtime_error("Файл не открылся");
+    }
+    f.seekg(0, ios::end);
+    long previousSize = f.tellg();
+    f.close();
+    return previousSize;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    const string PATH_ANSWERS = "Z:/бинарка/answer.bin";
+    const string PATH_REQUESTS = "Z:/бинарка/request.bin";
+    cout << "Введите ID клиента\n";
+    int client_id;
+    cin >> client_id;
+    while (true) {
+        Request request;
+        cout << "\nВведите имя (или напишите 0 если вы хотите выйти)\n";
+        string name;
+        cin >> name;
+        if (name == "0") {
+            return 1;
+        }
+
+        for (int i = 0; i < NAME_SIZE; i++) {
+            request.name[i] = '0';
+        }
+        for (int i = 0; i < name.length(); i++) {
+            request.name[i] = name[i];
+        }
+
+        cout << "Введите рост и вес\n";
+        cin >> request.height >> request.weight;
+        request.client_id = client_id;
+
+        long previousSizeOfAnswers = getSizeOfFile(PATH_ANSWERS);
+        writeRequest(PATH_REQUESTS, request);
+        Sleep(200);
+        long currentSizeOfAnswers = getSizeOfFile(PATH_ANSWERS);
+        Answer lastAnswer = getLastAnswer(PATH_ANSWERS, currentSizeOfAnswers);
+        while (previousSizeOfAnswers >= currentSizeOfAnswers || lastAnswer.client_id != request.client_id) {
+            cout << "Нет ответа\n";
+            Sleep(100);
+            lastAnswer = getLastAnswer(PATH_ANSWERS, currentSizeOfAnswers);
+            currentSizeOfAnswers = getSizeOfFile(PATH_ANSWERS);
+        }
+        cout << "Ваш индекс массы тела = " << lastAnswer.IMT << endl;
+    }
+}
---
+++ бинарка/Сервер/Сервер/Сервер.cpp
@@ -0,0 +1,86 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+#include <stdexcept>
+
+using namespace std;
+
+struct Request {
+	int client_id;
+	char name[25];
+	double height;
+	double weight;
+};
+
+struct Answer {
+	int client_id;
+	double IMT;
+};
+
+double getIMT(double height, double weight) {
+	return weight / (0.01 * height) / (0.01 * height);
+}
+
+void addAnswerToAnswers(string fileName, Request r) {
+	fstream f(fileName, ios::binary | ios::app);
+	if (!f) {
+		throw runtime_error("Файл не открылся");
+	}
+	double IMT = getIMT(r.height, r.weight);
+	Answer answer{ r.client_id, IMT };
+	f.write(reinterpret_cast<const char*>(&answer), sizeof(answer));
+	f.close();
+}
+
+Request getLastRequest(string filename, long currentSize) {
+	size_t size_of_request = sizeof(Request);
+	long last_request_position = currentSize - size_of_request;
+
+	ifstream file(filename, ios::binary);
+	if (!file) {
+		throw runtime_error("Файл не открылся");
+	}
+
+	Request last_request;
+	file.seekg(last_request_position);
+	file.read((char*)&last_request, size_of_request);
+
+	file.close();
+	return last_request;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	const string PATH_ANSWERS = "Z:/бинарка/answer.bin";
+	const string PATH_REQUESTS = "Z:/бинарка/request.bin";
+	cout << "Сервер работает\n";
+	fstream f(PATH_REQUESTS, ios::binary | ios::app);
+	if (!f) {
+		throw runtime_error("Файл не открылся");
+	}
+	f.seekg(0, ios::end);
+	long previousSize = f.tellg();
+	f.close();
+	while (true)
+	{
+		Sleep(100);
+		f.open(PATH_REQUESTS, ios::binary | ios::app);
+		if (!f) {
+			throw runtime_error("Файл не открылся");
+		}
+		f.seekg(0, ios::end);
+		long currentSize = f.tellg();
+		if (currentSize > previousSize) {
+			Request lastRequest = getLastRequest(PATH_REQUESTS, currentSize);
+			cout << "Получен запрос от клиента№" << lastRequest.client_id << endl;
+			addAnswerToAnswers(PATH_ANSWERS, lastRequest);
+			cout << "Отправлен ответ клиенту№" << lastRequest.client_id << endl;
+			previousSize = currentSize;
+		}
+		f.close();
+	}
+}
